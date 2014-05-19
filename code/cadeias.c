e09e2b519bf676c520dbb56a25cea94b9b7d6872
ricardobranco <28.ricardobranco@gmail.com>
2014-05-19 07:42:38 +0100

    Adicionado funções sobre strings

--- code/cadeias.c
+++ code/cadeias.c
@@ -35,3 +35,147 @@ int cad_ocorrencias(char c, char s[]){
 	return res;
 }
 
+int cad_procurar_str(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int i,j=0;
+	int restantes = len1;
+
+	for (i = 0; i < len2 && restantes;i++){
+		if(s1[j] == s2[i]){
+			restantes--;
+			j++;
+		}
+		else{
+			i=i-j;
+			j=0;
+			restantes=len1;
+		}
+	}
+	if(restantes)
+		return -1;
+	return i-j;
+}
+
+int cad_e_prefixo(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int i,flag;
+
+	if(len1 > len2)
+		return 0;
+
+	for(i = 0,flag=1;flag && i < len1;i++){
+		if(s1[i] != s2[i])
+			flag=0;
+	}
+
+	return flag;
+}
+
+int cad_e_sufixo(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int i,j,flag;
+
+	if(len1 > len2)
+		return 0;
+
+
+	for(i = 0,j = len2-len1,flag=1;flag && i < len1;i++,j++){
+		if(s1[i] != s2[j])
+			flag=0;
+	}
+
+	return flag;
+}
+
+int cad_iguais(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int i,flag;
+
+	if(len1 != len2)
+		return 0;
+
+
+	for(i = 0,flag=1;flag && i < len1;i++){
+		if(s1[i] != s2[i])
+			flag=0;
+	}
+
+	return flag;
+}
+
+int cad_primeiro(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int i,flag;
+
+	if(cad_iguais(s1,s2))
+		return 0;
+
+	for(i = 0,flag=1;flag && i < len1 && i< len2;i++){
+		if(s1[i] > s2[i])
+			flag=0;
+	}
+
+	if(flag){
+		flag=len1<len2?1:0;
+	}
+
+	return flag;
+}
+
+int cad_comparar(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int len2 = cad_tamanho(s2);
+	int res = 0;
+	int i;
+
+	for(i = 0; i<len1 && i<len2 && !res; i++){
+		if(s1[i]<s2[i])
+			res--;
+		else if(s1[i]>s2[i])
+			res++;
+	}
+
+	if(!res){
+		if(len1<len2)
+			res--;
+		else if(len1>len2)
+			res++;
+	}
+
+	return res;
+}
+
+int cad_um_dos(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int flag = 1;
+	int i;
+
+	for(i = 0; i<len1 && flag; i++){
+		if(cad_ocorrencias(s1[i],s2))
+			flag--;
+	}
+
+	if(!flag)
+		return i-1;
+	return -1;
+}
+
+int cad_nenhum_dos(char s1[], char s2[]){
+	int len1 = cad_tamanho(s1);
+	int flag = 1;
+	int i;
+
+	for(i = 0; i<len1 && flag; i++){
+		if(!cad_ocorrencias(s1[i],s2))
+			flag--;
+	}
+
+	if(!flag)
+		return i-1;
+	return -1;
+}
--- code/tproc.c
+++ code/tproc.c
@@ -2,13 +2,35 @@
 #include "cadeias.h"
 
 int main() {
-	printf("%d\n", cad_tamanho(""));
-	printf("%d\n", cad_procurar_car('b',"abaco"));
-	printf("%d\n", cad_procurar_car('z',"abaco"));
-	printf("%d\n", cad_ocorrencias('a',"abaco"));
-	printf("%d\n", cad_procurar_str("abc","a"));
-	printf("%d\n", cad_procurar_str("abc","abaco"));
-	printf("%d\n", cad_procurar_str("abc","olaabcd"));
+	printf("1: %d\n", cad_tamanho(""));
+	printf("2: %d\n", cad_procurar_car('b',"abaco"));
+	printf("3: %d\n", cad_procurar_car('z',"abaco"));
+	printf("4: %d\n", cad_ocorrencias('a',"abaco"));
+	printf("5: %d\n", cad_procurar_str("abc","a"));
+	printf("6: %d\n", cad_procurar_str("abc","abaco"));
+	printf("7: %d\n", cad_procurar_str("abc","olaabcd"));
+	printf("8: %d\n", cad_e_prefixo("abc","olaabcd"));
+	printf("9: %d\n", cad_e_prefixo("abc","olaabcd"));
+	printf("10: %d\n", cad_e_prefixo("abc","abcdefg"));
+	printf("11: %d\n", cad_e_prefixo("abc","ab"));
+	printf("12: %d\n", cad_e_sufixo("abc","ab"));
+	printf("13: %d\n", cad_e_sufixo("ab","abc"));
+	printf("14: %d\n", cad_e_sufixo("abc","cabc"));
+	printf("15: %d\n", cad_iguais("abc","abc"));
+	printf("16: %d\n", cad_iguais("abc","acb"));
+	printf("16: %d\n", cad_primeiro("abc","abc"));
+	printf("17: %d\n", cad_primeiro("abc","abcde"));
+	printf("18: %d\n", cad_primeiro("abcde","abc"));
+	printf("19: %d\n", cad_comparar("abc","abc"));
+	printf("20: %d\n", cad_comparar("abc","abcde"));
+	printf("21: %d\n", cad_comparar("abcde","abc"));
+	printf("22: %d\n", cad_um_dos("abcde","gtb"));
+	printf("23: %d\n", cad_um_dos("abc","def"));
+	printf("24: %d\n", cad_nenhum_dos("abcde","gtb"));
+	printf("25: %d\n", cad_nenhum_dos("abc","adebf"));
+
+
+
 
 
 	return 0;
