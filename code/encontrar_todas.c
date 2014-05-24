e35df2f91fe303dd2bb592b3fb7cd3a77ea6349e
ricardobranco <28.ricardobranco@gmail.com>
2014-05-24 13:03:00 +0100

    Tarefa 5: Procura em passo de cavalo

--- code/encontrar_todas.c
+++ code/encontrar_todas.c
@@ -39,6 +39,40 @@ void encontrar_todas_serpente(BOARD board,DICIONARIO dic, int x, int y, TREE res
 }
 
 
+void encontrar_todas_cavalo(BOARD board,DICIONARIO dic, int x, int y, TREE res, char* prefix){
+	int xmin,xmax,ymin,ymax;
+	int i,j;
+	int len;
+	char *newPrefix = NULL;
+	len = cad_tamanho(prefix);
+
+	if(palavra_existe(dic,prefix)){
+		if(!tree_search(res,prefix))
+			tree_add_palavra(res,prefix);
+	}
+
+	xmin = x-2;
+	xmax = x+2;
+	ymin = y-2;
+	ymax = y+2;
+	for(i = xmin;i <= xmax;i++){
+		for(j = ymin; j <= ymax;j++){
+			if(i >= 0 && i < board->nl && j >= 0 && j < board->nc && i != x && j != y){
+				if(((i == xmin || i == xmax) && ( j == y-1 || j == y+1))||((i == x-1 || i == x+1) && ( j == ymin || j == ymax))){
+					newPrefix= (char*) malloc(sizeof(char)*len+2);
+					cad_copiar(prefix,newPrefix);
+					newPrefix[len]=board->board[i][j];
+					newPrefix[len+1]='\0';
+					if(palavra_prefix(dic,newPrefix)){
+						encontrar_todas_cavalo(board,dic,i,j,res,newPrefix);
+					}
+				}
+			}
+		}
+	}
+}
+
+
 TREE encontrar_todas(BOARD board,DICIONARIO dic, char modo[]){
 	TREE res = tree_init();
 	char* prefix = (char*) malloc(sizeof(char));
@@ -48,12 +82,13 @@ TREE encontrar_todas(BOARD board,DICIONARIO dic, char modo[]){
 			sprintf(prefix,"%c",board->board[i][j]);
 			if(!cad_comparar(modo,"serpente")){
 				encontrar_todas_serpente(board,dic,i,j,res,prefix);
+			}else if(!cad_comparar(modo,"cavalo")){
+				encontrar_todas_cavalo(board,dic,i,j,res,prefix);
 			}
 		}
 	}
 	return res;
 }
-
 
 
 int main() {
